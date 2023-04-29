2c07004b3065f84472bb0303543357824c4cd71e
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-04-29 20:18:58 +0500

    Ping Pong client/server implemented

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,20 +1,20 @@
 use std::net::TcpStream;
 use std::io::{Write, BufReader};
 use std::io::prelude::*;
+use std::str;
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:7000").unwrap();
 
-    stream.write(b"Hello world").unwrap();
-    println!("Sent \"Hello world\", awaiting reply");
+    let ping_message = b"ping";
+    stream.write(ping_message).unwrap();
 
-    let buf_reader = BufReader::new(&mut stream);
+    println!("Sent \"ping\", awaiting reply");
 
-    let response: Vec<_> = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut buf_reader = BufReader::new(&mut stream);
 
-    println!("Request: {:#?}", response);
+    let mut pong_response: [u8; 4] = [0; 4];
+    buf_reader.read(&mut pong_response).unwrap();
+
+    println!("Response: {:#?}", str::from_utf8(&pong_response).unwrap());
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,26 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead}};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, Read, Write}};
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        println!("Incoming stream opened!");
         handle_stream(stream);
     }
 }
 
 fn handle_stream(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut buf_reader = BufReader::new(&mut stream);
+    
+    let mut ping_buffer = [0u8; 4];
+    buf_reader.read(&mut ping_buffer).unwrap();
  
-    println!("Request: {:#?}", http_request);
+    println!("Request: {:#?}", str::from_utf8(&ping_buffer).unwrap());
+
+    let pong_buffer = b"pong";
+    stream.write(pong_buffer).unwrap();
+
+    println!("Sent \"pong\"");
 }
